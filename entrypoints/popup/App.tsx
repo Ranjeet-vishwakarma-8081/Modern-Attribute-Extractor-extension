5f602e30ab58c41564ee6d05a70edc27f3f2aa9e
Ranjeet Vishwakarma <rv238502@gmail.com>
2025-06-14 13:14:35 +0530

    Styled the Popup UI in modern way

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -27,12 +27,12 @@ const App = () => {
 
   return (
     <div className="p-5 space-y-4 text-gray-800 shadow-md w-96">
-      <h1 className="text-xl font-bold text-center">Attribute Extractor</h1>
+      <h1 className="text-xl font-bold text-center">🧩 Attribute Extractor</h1>
       <p className="text-sm text-gray-500">
         Enable selection mode to extract attributes from any element on the
         page.
       </p>
-      <button
+      {/* <button
         type="button"
         onClick={handleToggleSelector}
         className={`w-full py-2 px-4 rounded-lg text-white font-semibold transition-colors duration-200 ${
@@ -42,7 +42,27 @@ const App = () => {
         }`}
       >
         {isActive ? "STOP" : "START"} SELECTOR
-      </button>
+      </button> */}
+      <div className="flex items-center justify-center gap-2">
+        <span className="text-sm font-medium text-gray-700 ">
+          Selector Mode
+        </span>
+        <label
+          htmlFor="toggle"
+          className="relative inline-flex items-center cursor-pointer"
+        >
+          <input
+            type="checkbox"
+            name="toggleMode"
+            id="toggle"
+            className="sr-only peer"
+            checked={isActive}
+            onChange={handleToggleSelector}
+          />
+          <div className="h-6 transition-all duration-300 bg-gray-300 rounded-full w-11 peer-focus:outline-none peer peer-checked:bg-green-500" />
+          <div className="absolute transition-transform duration-300 transform bg-white rounded-full left-1 top-1 size-4 peer-checked:translate-x-5"></div>
+        </label>
+      </div>
     </div>
   );
 };
